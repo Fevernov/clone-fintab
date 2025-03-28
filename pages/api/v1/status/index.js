5464bc6993ff3a7f53ce3aff0626b2141d09551a
Fevernov <104840238+Fevernov@users.noreply.github.com>
2025-03-28 14:12:06 +0000

    fixes `openedConections` not showing on endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -19,7 +19,7 @@ async function status(request, response) {
   });
 
   const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows.lenght;
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
