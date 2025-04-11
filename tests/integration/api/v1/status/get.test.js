e011698dead272823bbb689613a23759e82d4c16
Fevernov <104840238+Fevernov@users.noreply.github.com>
2025-04-11 13:21:34 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,20 +4,32 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status"),
+  () => {
+    describe("Anonymous user"),
+      () => {
+        test("Retrieving current system status", async () => {
+          const response = await fetch("http://localhost:3000/api/v1/status");
+          expect(response.status).toBe(200);
 
-  const responseBody = await response.json({});
+          const responseBody = await response.json({});
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  // end of update
+          const parsedUpdatedAt = new Date(
+            responseBody.updated_at,
+          ).toISOString();
+          expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+          // end of update
 
-  // version of postgres
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  // max connections
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  // opened connections
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-});
+          // version of postgres
+          expect(responseBody.dependencies.database.version).toEqual("16.0");
+          // max connections
+          expect(responseBody.dependencies.database.max_connections).toEqual(
+            100,
+          );
+          // opened connections
+          expect(responseBody.dependencies.database.opened_connections).toEqual(
+            1,
+          );
+        });
+      };
+  };
