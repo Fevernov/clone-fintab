b641a98a001be080b9ad5fb00529d9fceb621e2c
Fevernov <104840238+Fevernov@users.noreply.github.com>
2025-04-15 14:21:59 +0000

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,56 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  return (
+    <>
+      <h1>STATUS</h1>
+      <UpdatedAt isLoading={isLoading} data={data} />
+      <DatabaseStatus isLoading={isLoading} data={data} />
+    </>
+  );
+}
+
+function UpdatedAt({ isLoading, data }) {
+  let UpdatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    UpdatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Última atualização: {UpdatedAtText}</div>;
+}
+
+function DatabaseStatus({ isLoading, data }) {
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
+  );
+}
