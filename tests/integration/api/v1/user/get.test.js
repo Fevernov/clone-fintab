28a06922412a8d1ef8df49c8a6a5239a4dfc1df9
Fevernov <jeanvitor.joi@gmail.com>
2026-09-24 01:10:24 -0300

    feat: add `sameSite=Lax` to `controller.setSessionCookie()`

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -75,6 +75,7 @@ describe("GET /api/v1/user", () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: "/",
         httpOnly: true,
+        sameSite: "lax",
       });
     });
 
@@ -139,6 +140,7 @@ describe("GET /api/v1/user", () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: "/",
         httpOnly: true,
+        sameSite: "lax",
       });
     });
 
